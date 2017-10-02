25bd3c8cec834d858fee272826d920bf349786da
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2017-10-01 20:49:15 -0400

    Add max_velocity and kp_turn params Refactor code for readability

--- iarc_main/scripts/follow.py
+++ iarc_main/scripts/follow.py
@@ -10,10 +10,10 @@ class FollowBehavior:
         rospy.init_node('FollowBehavior')
         self.pub = rospy.Publisher('/follow/cmd_vel', Twist, queue_size=10)
         self.tf = TransformListener()
-        
+
         # Wait until the transformListener initialize
         rospy.sleep(0.5)
-  
+
     def run(self):
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
@@ -27,23 +27,28 @@ class FollowBehavior:
 	            cmd = Twist()
 
 	            # maxvelocity of our drone
-	            maxvelocity = 1
+    	        maxvelocity = rospy.get_param('~max_velocity', 1.0)
+                kpturn = rospy.get_param('~kp_turn', 2)
 
-	            # diagonalvelocity = the velocity we would get if we use position[0] and position[1]
-	            diagonalvelocity = math.sqrt(position[0]**2 + position[1]**2)
+	            # diagonalvelocity = the velocity we would get if we use position[0] and position[1
+                diagonalvelocity = math.sqrt(position[0]**2 + position[1]**2)
 
 	            # Scale down our velocity_x and velocity_y so that our diagonalvelocity won't exceed maxvelocity
-	            # But if diagonalvelocity is already < max_velocity then use diagonalvelocity instead (slow down when approach nearer)
-	            cmd.linear.x = position[0] if diagonalvelocity < maxvelocity else position[0] / diagonalvelocity * maxvelocity
-	            cmd.linear.y = position[1] if diagonalvelocity < maxvelocity else position[1] / diagonalvelocity * maxvelocity
-	            
-	            # Turn our drone to the position_x/y of the target
-	            cmd.angular.z = math.atan2(position[1], position[0])
+	            # But if diagonalvelocity_ is already < max_velocity then use diagonalvelocity instead (slow down when approach nearer)
+                if diagonalvelocity < maxvelocity:
+                    cmd.linear.x = position[0]
+                    cmd.linear.y = position[1]
+                else:
+                    cmd.linear.x = position[0] / diagonalvelocity * maxvelocity
+                    cmd.linear.y = position[1] / diagonalvelocity * maxvelocity
 
-	            # Publish the motion
-	            self.pub.publish(cmd)
+                # Turn our drone to the position_x/y of the target
+                cmd.angular.z = kpturn * math.atan2(position[1], position[0])
 
-        	r.sleep()
+	            # Publish the
+                self.pub.publish(cmd)
+
+                r.sleep()
 
 if __name__ == '__main__':
     ex = FollowBehavior()
