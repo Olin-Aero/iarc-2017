9e35fd7a0a786c8d2739185c0d531d9e2252e2e0
Eric Miller <eric@legoaces.org>
2017-10-08 18:23:18 -0400

    Simplified the arbiter some

--- iarc_arbiter/scripts/arbiter.py
+++ iarc_arbiter/scripts/arbiter.py
@@ -11,11 +11,11 @@ rospy.init_node('arbiter')
 
 class Arbiter:
     def __init__(self):
-        self.null_behavior = Behavior(self, namespace='', name='null', friendly_name='UndefinedBehavior')
+        self.null_behavior = Behavior(self.process_command, namespace='', name='zero', friendly_name='StopBehavior')
 
-        self.behaviors = [self.null_behavior]
-        self.active_behavior = self.null_behavior
-        self.choose_leader()
+        self.behaviors = {'zero': self.null_behavior}
+        self.active_behavior_name = ''
+        self.set_active_behavior('zero')
 
         # Transformers are functions capable of processing incoming data in a variety of formats.
         self.transformers = {
@@ -28,13 +28,22 @@ class Arbiter:
         self.secondaries = []
 
         self.vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=0)
-        self.takeoff_pub = rospy.Publisher('/ardrone/takeoff', Empty, queue_size=10)
-        self.land_pub = rospy.Publisher('/ardrone/land', Empty, queue_size=10)
+        self.takeoff_pub = rospy.Publisher('/ardrone/takeoff', Empty, queue_size=0)
+        self.land_pub = rospy.Publisher('/ardrone/land', Empty, queue_size=0)
 
         self.status_pub = rospy.Publisher('status', String, queue_size=10)
         self.active_pub = rospy.Publisher('active_behavior', String, queue_size=10)
 
         rospy.Service('register', Register, self.handle_register)
+
+        rospy.Subscriber('activate', String, self.handle_activate)
+
+    def handle_activate(self, msg):
+        self.set_active_behavior(msg.data)
+
+    def set_active_behavior(self, name):
+        self.active_behavior_name = name
+        rospy.loginfo_throttle(1.0, '{} selected as active behavior'.format(name))
 
     def handle_register(self, req):
         """
@@ -45,23 +54,34 @@ class Arbiter:
             rospy.logerr("Behavior cannot be created with empty namespace")
             return RegisterResponse()
 
-        behavior = Behavior(self, req.namespace, req.name, req.pretty_name)
-        self.behaviors.append(behavior)
+        behavior = Behavior(self.process_command, req.namespace, req.name, req.pretty_name)
+        behavior.subscribe(self.transformers)
+        self.behaviors[req.name] = behavior
         rospy.loginfo("Created behavior {}".format(behavior))
 
         return RegisterResponse(name=behavior.name)
 
-    def process_command(self, behavior, cmd):
+    def process_command(self, behavior, topic, raw_cmd):
         """
         process_command gets called after a message gets received from the currently active behavior.
 
-        :param behavior: The behavior initiating the request
-        :type cmd: transformers.Command
+        :param (str) behavior: The name of the behavior initiating the command
+        :param (str) topic: The topic (without namespace) to which the command was sent
+        :type raw_cmd: ROS message
         """
+        if behavior != self.active_behavior_name:
+            return False
 
+        _, transformer = self.transformers[topic]
+
+        # Convert to a transformers.Command
+        cmd = transformer(raw_cmd)
+
+        # Apply secondary behaviors
         for func in self.secondaries:
             cmd = func(cmd)
 
+        # Publish the result to the ROS network
         if cmd.takeoff:
             self.takeoff_pub.publish(Empty())
             self.vel_pub.publish(Twist())
@@ -71,14 +91,11 @@ class Arbiter:
         else:
             self.vel_pub.publish(cmd.vel)
 
-        self.active_pub.publish(String(behavior.name))
+        # Log it
+        self.active_pub.publish(String(behavior))
         rospy.loginfo_throttle(1, "Command published by {}".format(behavior.name))
 
-    def choose_leader(self):
-        # TODO: placeholder, make votes and stuff count
-        self.active_behavior.is_leader = False
-        self.active_behavior = self.behaviors[-1]
-        self.active_behavior.is_leader = True
+        return True
 
     def publish_status(self):
         self.status_pub.publish(String(str(self.behaviors)))
@@ -87,7 +104,6 @@ class Arbiter:
         r = rospy.Rate(20)
 
         while not rospy.is_shutdown():
-            self.choose_leader()
             self.publish_status()
             self.null_behavior.handle_message('cmd_vel', Twist())
 
@@ -95,43 +111,44 @@ class Arbiter:
 
 
 class Behavior:
-    def __init__(self, arbiter, namespace, name=None, friendly_name=None, intrinsic_vote=0.0):
+    def __init__(self, callback, namespace, name=None, friendly_name=None):
         """
-        :param arbiter: The Arbiter to which this belongs
+        :param callback: A function that takes this behavior and the standardized message as arguments
         :param namespace: If empty, the behavior is treated as being internal to the Arbiter.
         :param name: The name used to refer to this elsewhere in ROS
         :param friendly_name: The name displayed, in CapitalCamelCase
-        :param intrinsic_vote: A "default" vote associated with this behavior
         """
         if namespace and (namespace[0] != '/'):
             namespace = '/' + namespace
+
         if not name:
             name = namespace.strip('/')
+
         if not friendly_name:
             friendly_name = name
 
         self.name = name
-        self.arbiter = arbiter
+        self.callback = callback
         self.namespace = namespace
         self.friendly_name = friendly_name
-        self.intrinsic_vote = intrinsic_vote
         self.subscribers = dict()
 
-        self.is_leader = False
         self.last_msg_time = rospy.Time(0)
 
-        self.subscribe()
-
     def handle_message(self, topic, msg):
-        msg_type, transformer = self.arbiter.transformers[topic]
+            self.last_msg_time = rospy.Time.now()
+            self.callback(self.name, topic, msg)
 
-        self.last_msg_time = rospy.Time.now()
-        if self.is_leader:
-            standardized = transformer(msg)
-            self.arbiter.process_command(self, standardized)
+    def subscribe(self, transformers):
+        """
+        Subscribes to the topics specified by transformers from the namespace.
+        :param transformers: map{topic name : (Type, Transformer)
+        :return:
+        """
+        if not self.namespace:
+            return
 
-    def subscribe(self):
-        for (topic, (msg_type, transformer)) in enumerate(self.arbiter.transformers):
+        for (topic, (msg_type, _)) in enumerate(transformers):
 
             def callback(msg):
                 self.handle_message(topic, msg)
