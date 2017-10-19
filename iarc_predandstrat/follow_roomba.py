6fff4dbdec4da8f9e24444684ef7b5ce563474f4
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2017-10-19 14:10:06 -0400

    Create follow behavior function linked with arbiter

--- iarc_predandstrat/follow_roomba.py
+++ iarc_predandstrat/follow_roomba.py
@@ -1,94 +1,52 @@
-import math
 import rospy
-from geometry_msgs.msg import Twist
-from tf import TransformListener
+from geometry_msgs.msg import Twist, PoseStamped
+from iarc_arbiter.msg import RegisterBehavior
+from std_msgs.msg import String
 
 
-class RoombaState(object):
-    def __init__(self, pos=None, heading=0, clock=0):
-        if pos is None:
-            pos = [0, 0]
-        self.pos = pos
-        self.heading = heading  # number from 0 to 2*pi
-        self.clock = clock
+class Roomba(object):
+    def __init__(self, frame_id=None):
+        """
+        This class will be re-defined later for everyone to use in predandstrat
+        :param frame_id: id used in header of PoseStamped
+        """
+        self.frame_id = frame_id
 
 
-def follow_roomba(roomba_state, relative_x=0, relative_y=0, aborted=False):
-    if not aborted:
-        # Do something
-        pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-        r = rospy.Rate(20)
-        tf = TransformListener()
-        # Wait until the transformListener initialize
-        rospy.sleep(0.5)
+def follow_roomba(roomba, des_x=0, des_y=0, des_z=0):
+    """
+    Follow roomba X with desired position des_x, des_y
+    :param roomba: an roomba object
+    :param des_x: desired position x
+    :param des_y: desired position y
+    :param aborted: if aborted, stop following
+    :return:
+    """
+    test_msg = PoseStamped()
+    test_msg.pose.position.x = des_x
+    test_msg.pose.position.y = des_y
+    test_msg.pose.position.z = des_z
+    test_msg.header.frame_id = roomba.frame_id
 
-        maxvelocity = rospy.get_param('~max_velocity', 1.0)  # Max velocity the drone can reach
-        kpturn = rospy.get_param('~kp_turn', 1.0)  # Proportional for turning
-        kp = rospy.get_param('~kp', 1.0)  # Proportional
-        ki = rospy.get_param('~ki', 0.2)  # Integral
-        kd = rospy.get_param('~kd', 0.0)  # Derivative: kd is not currently used
+    r = rospy.Rate(10)
+    pub = rospy.Publisher('/follow/cmd_pos', PoseStamped, queue_size=10)
 
-        last_time = rospy.Time.now()
-        integral_x = 0.0
-        previous_error_x = 0.0
-        integral_y = 0.0
-        previous_error_y = 0.0
-        while not rospy.is_shutdown() and not aborted:
-            cmd = Twist()
-
-            t = tf.getLatestCommonTime("drone", "map")
-            position, quaternion = tf.lookupTransform("drone", "map", t)
-            des_x = position[0] - roomba_state.pos[0] + relative_x
-            des_y = position[1] - roomba_state.pos[1] + relative_y
-            if des_y + des_y == 0:
-                return True
-            des_pos = [des_x, des_y]
-
-            now = rospy.Time.now()
-            dt = (now - last_time).to_sec()
-            last_time = now
-
-            # In case something weird happens with the loop freezing
-            dt = min(dt, 0.5)
-            dt = max(dt, 0.01)
-
-            # Calculate DIP velocity_x
-            error_x = des_pos[0]
-            integral_x = integral_x + error_x * dt
-            derivative_x = (error_x - previous_error_x) / dt
-            previous_error_x = error_x
-            dip_x = kp * error_x + ki * integral_x + kd * derivative_x
-
-            # Calculate DIP velocity_y
-            error_y = des_pos[1]
-            integral_y = integral_y + error_y * dt
-            derivative_y = (error_y - previous_error_y) / dt
-            previous_error_y = error_y
-            dip_y = kp * error_y + ki * integral_y + kd * derivative_y
-
-            # Combined velocity
-            dip_diagonal = math.sqrt(dip_x ** 2 + dip_y ** 2)
-
-            if dip_diagonal < maxvelocity:
-                # If dip_diagonal is already < max_velocity
-                # use dip velocity
-                cmd.linear.x = dip_x
-                cmd.linear.y = dip_y
-            else:
-                # Else use max velocity
-                diagonalvelocity = math.sqrt(des_pos[0] ** 2 + des_pos[1] ** 2)
-                cmd.linear.x = des_pos[0] / diagonalvelocity * maxvelocity
-                cmd.linear.y = des_pos[1] / diagonalvelocity * maxvelocity
-
-            cmd.angular.z = kpturn * math.atan2(cmd.linear.y, cmd.linear.x)
-            pub.publish(cmd)
-            r.sleep()
-
-    else:  # Action Interrupted
-        return False
+    while not rospy.is_shutdown():
+        r.sleep()
+        pub.publish(test_msg)
 
 
 if __name__ == '__main__':
+    # Init FollowBehavior node
     rospy.init_node('FollowBehavior')
-    roomba_state = RoombaState([10, 10], 0, 0)
-    print follow_roomba(roomba_state, 0, 0, False)
+
+    # Register this behavior (FollowBehavior) to the arbiter
+    rospy.Publisher('/arbiter/register', RegisterBehavior, latch=True, queue_size=10).publish(name='follow')
+
+    # TODO: For testing purposes only, will delete those lines of code below
+    # This command tell the arbiter that this behavior will take command from now on and abort other behaviors
+    rospy.Publisher('/arbiter/activate_behavior', String, latch=True, queue_size=10).publish('follow')
+
+    # Create a target object just for testing
+    target = Roomba("target0")
+    follow_roomba(roomba=target, des_x=0, des_y=0)
