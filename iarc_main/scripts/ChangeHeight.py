5610deb3123949bac12ef40115b0a0402ae64ae5
nestill <nathan.estill@students.olin.edu>
2017-10-22 20:33:52 -0400

    updating code

--- iarc_main/scripts/ChangeHeight.py
+++ iarc_main/scripts/ChangeHeight.py
@@ -9,53 +9,37 @@ from geometry_msgs.msg import Twist, Vector3
 
 class ChangingHeight:
     def __init__(self):
-        #self.listener = tf.TransformListener()
-        self.ActualHeight = 4
         self.heightPub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.cmdHeight = rospy.Publisher('/cmd_height', Float64, queue_size=10)
         rospy.init_node('Example')        
-        rospy.Subscriber("/drone/height", Float64, self.recordActualHeight)
+        rospy.Subscriber("/drone/height", Float64, self.callback)
         time.sleep(1)
-    def user_input_callback(self, msg):
-        print msg.data
-  #  def recordNeededHeight(self,msg):
-   #     self.neededHeight = msg.data
-        print ("me too")
-    def recordActualHeight(self,msg):
+    def callback(self, msg):
         self.actualHeight = msg.data
-    def run(self):
-        outing = Float64()
-        outing.data = 2.0
-        self.cmdHeight.publish(outing)   
-        #self.changeHeight()
     def changeHeight(self,height):
         heightRequested = height
-        currentHeight = self.neededHeight
+        currentHeight = self.actualHeight
         if(currentHeight < heightRequested - 0.05):
             output = Twist()
             output.linear = Vector3(0, 0, 1)
             output.angular = Vector3(0,0,0)
             self.heightPub.publish(output)
-            print ("I'm here")
         elif(currentHeight > heightRequested + 0.05):
             output = Twist()
             output.linear = Vector3(0, 0, -1)
             output.angular = Vector3(0,0,0)
             self.heightPub.publish(output)
-            print ("I'm there")
         else:
             output = Twist()
             output.linear = Vector3(0, 0, 0)
             output.angular = Vector3(0,0,0)
             self.heightPub.publish(output)
-            print ("I'm over here")
-  #  def toGroundFloor():
-       # changeHeight(0)
-   # def tapRoomba():
-        #changeHeight(0.92)
-   # def toCruising():
-        #changeHeight(2.8)
+    def heightLoop(self,height):
+        while(True):
+            self.changeHeight(height)
+            rospy.sleep(.1)
+            print(self.actualHeight)
 if __name__ == '__main__':
     ex = ChangingHeight()
-    ex.run()
+    ex.heightLoop(3.0)
     rospy.spin()
