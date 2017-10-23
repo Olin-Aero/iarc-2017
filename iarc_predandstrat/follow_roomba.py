1ef235f6ee1130a4fe339b504aeff7055f2a5486
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2017-10-22 21:14:59 -0400

    Bring a for loop outside of follow_roomba function

--- iarc_predandstrat/follow_roomba.py
+++ iarc_predandstrat/follow_roomba.py
@@ -13,38 +13,39 @@ class Roomba(object):
         self.frame_id = frame_id
 
 
-def follow_roomba(roomba, des_x=0, des_y=0, des_z=0):
+def follow_roomba(roomba, des_x=0, des_y=0, des_z=0, pub=None):
     """
     Follow roomba X with desired position des_x, des_y
     :param roomba: an roomba object
     :param des_x: desired position x
     :param des_y: desired position y
+    :param des_z: desired position z
     """
-    test_msg = PoseStamped()
-    test_msg.pose.position.x = des_x
-    test_msg.pose.position.y = des_y
-    test_msg.pose.position.z = des_z
-    test_msg.header.frame_id = roomba.frame_id
 
-    r = rospy.Rate(10)
-    pub = rospy.Publisher('/follow/cmd_pos', PoseStamped, queue_size=10)
-
-    while not rospy.is_shutdown():
-        r.sleep()
-        pub.publish(test_msg)
+    pose_stamped = PoseStamped()
+    pose_stamped.pose.position.x = des_x
+    pose_stamped.pose.position.y = des_y
+    pose_stamped.pose.position.z = des_z
+    pose_stamped.header.frame_id = roomba.frame_id
+    if pub is not None:
+        pub.publish(pose_stamped)
 
 
 if __name__ == '__main__':
+    # Create a target object just for testing
+    target = Roomba("target0")
+
     # Init FollowBehavior node
     rospy.init_node('FollowBehavior')
 
     # Register this behavior (FollowBehavior) to the arbiter
     rospy.Publisher('/arbiter/register', RegisterBehavior, latch=True, queue_size=10).publish(name='follow')
 
-    # TODO: For testing purposes only, will delete those lines of code below
     # This command tell the arbiter that this behavior will take command from now on and abort other behaviors
     rospy.Publisher('/arbiter/activate_behavior', String, latch=True, queue_size=10).publish('follow')
 
-    # Create a target object just for testing
-    target = Roomba("target0")
-    follow_roomba(roomba=target, des_x=0, des_y=0)
+    # Create a follow publisher
+    pub = rospy.Publisher('/follow/cmd_pos', PoseStamped, queue_size=10)
+
+    while not rospy.is_shutdown():
+        follow_roomba(roomba=target, des_x=0, des_y=0, pub=pub)
