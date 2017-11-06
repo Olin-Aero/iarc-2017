3b8d0d0f4f8abc85f96d090b983fbd177e91b98b
Eric Miller <eric@legoaces.org>
2017-11-05 23:11:56 -0500

    Cleaned up code, added comments, and changed topic name to visible_roombas

--- iarc_vision/scripts/apriltag_rebroadcast.py
+++ iarc_vision/scripts/apriltag_rebroadcast.py
@@ -1,27 +1,42 @@
 #!/usr/bin/env python2
 
 import rospy
+
 from std_msgs.msg import Header
-from iarc_main.msg import RoombaSighting, Roomba
 from apriltags_ros.msg import AprilTagDetectionArray, AprilTagDetection
 from geometry_msgs.msg import PoseWithCovarianceStamped, PoseWithCovariance, PointStamped, Point
 
+from iarc_main.msg import RoombaSighting, Roomba
+
 
 class TagTransformer(object):
-    def __init__(self, linear_covariance=0.1, angular_covariance=0.5):
+    """
+    A TagTransformer is responsible for transforming detections by the apriltags_ros library
+    into RoombaSighting messages for processing by the rest of the stack.
+    """
+    def __init__(self, linear_covariance=0.1, angular_covariance=0.5, tf_frame='map', camera_fov=1.0):
+        """
+        :param linear_covariance: (meters) the linear covariance associated with a tag detection
+        :param angular_covariance: (radians) the angular covariance of a tag
+        :param tf_frame (str): The initial TF frame name to use before any detections come in.
+        :param camera_fov (float): The FOV radius of the camera, in meters.
+        """
         rospy.Subscriber('tag_detections', AprilTagDetectionArray, self.on_tags)
-        self.pub = rospy.Publisher('seen_roombas', RoombaSighting, queue_size=0)
+        self.pub = rospy.Publisher('visible_roombas', RoombaSighting, queue_size=0)
 
         cov = [0] * 36
         cov[0] = cov[7] = cov[14] = linear_covariance
         cov[21] = cov[28] = cov[35] = angular_covariance
         self.covariance = cov
 
-        self.tf_frame = 'map'  # TF frame is unknown until we get a message with a tag in it
+        self.tf_frame = tf_frame  # TF frame is unknown until we get a message with a tag in it
+        self.fov = camera_fov
 
     def on_tags(self, msg):
         """
-        Handler for AprilTag detection
+        Handler for AprilTag detection.
+        Converts AprilTagDetectionArray to a RoombaSighting object, then publishes it.
+
         :type msg: AprilTagDetectionArray
         """
         sighting = RoombaSighting()
@@ -36,7 +51,7 @@ class TagTransformer(object):
                 )
             )
 
-            roomba.type = Roomba.GREEN
+            roomba.type = [Roomba.GREEN, Roomba.RED][tag.id % 2]
 
             roomba.last_seen = tag.pose.header.stamp
 
@@ -45,7 +60,9 @@ class TagTransformer(object):
 
             self.tf_frame = tag.pose.header.frame_id
 
-        # TODO: Use tf to get camera position at capture time
+        # TODO: Use tf to get camera position and altitude at capture time for better FOV estimate
+        # This potentially requires subscribing to tag_detections_poses as well, to get timestamps of empty
+        # detection events.
         sighting.fov_center = PointStamped(
             header=Header(
                 stamp=rospy.Time(0),
@@ -53,7 +70,7 @@ class TagTransformer(object):
             ),
             point=Point()
         )
-        sighting.fov_radius = 1.0
+        sighting.fov_radius = self.fov
 
         self.pub.publish(sighting)
 
