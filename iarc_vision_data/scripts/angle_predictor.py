8a5ba8764420e1a8975f16fbf9b517d683d5336a
nathanyee <nathaniel.yee@students.olin.edu>
2017-11-08 20:04:30 -0500

    made input shape generalizable to model config

--- iarc_vision_data/scripts/angle_predictor.py
+++ iarc_vision_data/scripts/angle_predictor.py
@@ -9,6 +9,7 @@ Model weights/config: (figuring out how to do without saving on git)
 """
 
 #import modules
+import os
 import numpy as np
 from PIL import Image
 from keras.models import load_model
@@ -16,6 +17,7 @@ from keras.applications.mobilenet import relu6, DepthwiseConv2D
 import keras.backend as K
 import tensorflow as tf
 from scipy.misc import imresize
+
 
 class AnglePredictor(object):
     def __init__(self, model_path='mobilenet_finetune.hdf5', verbose=2, allocated_mem=.6):
@@ -35,38 +37,8 @@ class AnglePredictor(object):
 
         # Initialize AnglePredictor
         self.limit_mem()
-        self.model = self.init_model()
-
-
-    def vec_to_angle(self, vec):
-        """
-        Turns a vector into its corresponding angle
-
-        :param vec (ndarray): two item object that contains the [x, y] components of a vector
-        """
-        return np.degrees(np.arctan2(vec[1], vec[0]))
-
-
-    def predict_angle(self, img):
-        """
-        Predicts the angle of a roomba relative to the drone
-
-        :param img (ndarray): an image in RGB format. Ex. (144, 168, 3)
-
-        :returns angle (float): the angle from the top of the image (counter clockwise positive, clockwise negative). A roomba at zero degrees looks like it is facing directly up in the image.
-        """
-        if img.shape != (128,128,3):
-            img = imresize(img, (128,128))
-        if len(img.shape) == 3:
-            img = np.expand_dims(img, axis=0) # format img for network prediction (1, 128, 128, 3)
-        else:
-            print "Image shape is {} - please enter image with 3 dimenions".format(img.shape)
-
-        y_pred = model.predict(x_test)
-        angle = self.vec_to_angle(y_pred)
-        if self.verbose >= 2:
-            print "Predicted angle is: {} degrees".format(angle)
-        return angle
+        self.init_model()
+        self.get_input_shape()
 
 
     def limit_mem(self):
@@ -91,6 +63,45 @@ class AnglePredictor(object):
         self.model = load_model(self.model_path,
                                 custom_objects={'relu6': relu6,
                                 'DepthwiseConv2D': DepthwiseConv2D})
+
+
+    def get_input_shape(self):
+        """ gets the input shape of the model for image size checking """
+        self.input_shape = tuple([getattr(dim, 'value') for dim in self.model.input.shape.dims[1:]])
+
+
+    def vec_to_angle(self, vec):
+        """
+        Turns a vector into its corresponding angle
+
+        :param vec (ndarray): two item object that contains the [x, y] components of a vector
+        """
+        return np.degrees(np.arctan2(vec[1], vec[0]))
+
+
+    def predict_angle(self, img):
+        """
+        Predicts the angle of a roomba relative to the drone
+
+        :param img (ndarray): an image in RGB format. Ex. (144, 168, 3)
+
+        :returns angle (float): the angle from the top of the image (counter clockwise positive, clockwise negative). A roomba at zero degrees looks like it is facing directly up in the image.
+        """
+        if img.shape != self.input_shape:
+            img = imresize(img, self.input_shape[:2])
+        if len(img.shape) == 3:
+            img = np.expand_dims(img, axis=0) # format img for network prediction (1, 128, 128, 3)
+        else:
+            print "Image shape is {} - please enter image with 3 dimenions".format(img.shape)
+
+        y_pred = model.predict(x_test)
+        angle = self.vec_to_angle(y_pred)
+        if self.verbose >= 2:
+            print "Predicted angle is: {} degrees".format(angle)
+        return angle
+
+
+
 
 
 if __name__ == '__main__':
