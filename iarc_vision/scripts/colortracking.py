84279a48dc850a51460aadfa6ea5505eb69c461d
nathanestill <nathan.estill@students.olin.edu>
2018-03-03 13:18:11 -0500

    adding color tracking file

--- iarc_vision/scripts/colortracking.py
+++ iarc_vision/scripts/colortracking.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 """
 How to Use:
@@ -12,20 +12,23 @@ Owner:
 
 
 import rospy
+import sys
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 import cv2
 
-class ColorTracker(object):
+class ColorTrackerROS(object):
     def __init__(self):
-    	rospy.init_node('color_tracker')
+        rospy.init_node('color_tracker')
         self.cv_image = None     # the latest image from the camera
         self.bridge = CvBridge() # used to convert ROS messages to OpenCV
         rospy.Subscriber("/usb_cam/image_raw", Image, self.process_image)
         print "Initializing Color Tracker"
         cv2.namedWindow('preview_window')
         cv2.namedWindow('binary')
+
+        self.tracker = ColorTracker()
 
     def process_image(self, msg):
         """
@@ -36,6 +39,11 @@ class ColorTracker(object):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
             # do image processing here
+            boxes = self.tracker.find_bounding_boxes(cv_image)
+            
+            # TODO: Do something with the boxes
+            # TODO: make sure it doesn't get too far behind
+
             binary_image = cv2.inRange(cv_image, (128,128,128),(255,255,255))
             self.binary_image = binary_image
             self.cv_image = cv_image
@@ -55,6 +63,25 @@ class ColorTracker(object):
                 cv2.waitKey(5)
             r.sleep()
 
+class ColorTracker(object):
+    def __init__(self):
+        pass
+
+    def find_bounding_boxes(self, image):
+        """
+        Input: OpenCV image (numpy array)
+        Output: List of bounding boxes (topleft, bottomright, isRed)
+        """
+        # TODO: Make this function
+        return [((20,20),(30,40),True)]
+
+def testImageFromFile(filename):
+    tracker = ColorTracker()
+    # TODO: Load image
+    tracker.find_bounding_boxes("TODO: This")
+    # TODO: Display result
+
 if __name__ == '__main__':
-	colortracker = ColorTracker()
-	colortracker.run()
+#	colortracker = ColorTrackerROS()
+#	colortracker.run()
+    testImageFromFile(sys.argv[1])
