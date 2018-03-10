9ce7413bc3f5c0507662730da862b84e905ac7d9
minhkhang1795 <13646394+minhkhang1795@users.noreply.github.com>
2018-03-10 15:33:06 -0500

    Add green detection

--- iarc_vision/scripts/colortracking.py
+++ iarc_vision/scripts/colortracking.py
@@ -10,7 +10,6 @@ Owner:
 
 """
 
-
 import rospy
 import sys
 from sensor_msgs.msg import Image
@@ -18,15 +17,15 @@ from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 import cv2
 
+
 class ColorTrackerROS(object):
     def __init__(self):
         rospy.init_node('color_tracker')
 
         self.tracker = ColorTracker()
 
-
-        self.cv_image = self.processed_image = None     # the latest image from the camera
-        self.bridge = CvBridge() # used to convert ROS messages to OpenCV
+        self.cv_image = self.processed_image = None  # the latest image from the camera
+        self.bridge = CvBridge()  # used to convert ROS messages to OpenCV
         rospy.Subscriber("/ardrone/bottom/image_raw", Image, self.process_image)
         print("Initializing Color Tracker")
         cv2.namedWindow('preview_window')
@@ -42,12 +41,12 @@ class ColorTrackerROS(object):
             self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
             # do image processing here
             self.boxes, self.processed_image = self.tracker.find_bounding_boxes(self.cv_image, display=False)
-            
+
             # TODO: Do something with the boxes
             # TODO: make sure it doesn't get too far behind
 
-            #self.binary_image = binary_image
-            #self.cv_image = cv_image
+            # self.binary_image = binary_image
+            # self.cv_image = cv_image
         except CvBridgeError as e:
             print "Error loading image"
             print(e)
@@ -64,59 +63,80 @@ class ColorTrackerROS(object):
                 cv2.waitKey(5)
             r.sleep()
 
+
 class ColorTracker(object):
     def __init__(self):
         self.red_lower_bound = 0
+
     def find_bounding_boxes(self, image, display=True):
         """
         Input: OpenCV image (numpy array)
         Output: List of bounding boxes (topleft, b;;ottomright, isRed)
         """
         # TODO: Make this function
-        self.cv_image = image
-        hsv_image = cv2.cvtColor(self.cv_image, cv2.COLOR_BGR2HSV)
-        binary_image1 = cv2.inRange(hsv_image, np.array([0,100,0],dtype = "uint8"),np.array([10,255,255],dtype = "uint8"))
-        binary_image2 = cv2.inRange(hsv_image, np.array([170,100,0],dtype = "uint8"),np.array([180,255,255],dtype = "uint8"))
-        binary_image3 = cv2.inRange(self.cv_image, np.array([80,80,250],dtype = "uint8"),np.array([220,230,255],dtype = "uint8"))
-        binary_image4 = cv2.bitwise_or(binary_image1,binary_image2)
-        binary_image5 = cv2.bitwise_or(binary_image3,binary_image4)
-        kernel = np.ones((5,5),np.uint8)
-        binary_image6 = cv2.morphologyEx(binary_image5, cv2.MORPH_OPEN, kernel)
-        #cv2.imshow("images",binary_image)
-        #cv2.waitKey(0)
-        ret,thresh = cv2.threshold(binary_image3,127,255,0)
-        image,contours,hierarchy = cv2.findContours(thresh,1,2)
-        #cnt = countours[0]
+        # Get bounding boxes around red and green rectangles
+        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        red_image = self.get_red_bounding_boxes(hsv_image, image)
+        green_image = self.get_green_bounding_boxes(hsv_image, image)
+        binary_image = cv2.bitwise_or(green_image, green_image)
+
+        # Remove noise
+        kernel = np.ones((5, 5), np.uint8)
+        binary_image = cv2.morphologyEx(binary_image, cv2.MORPH_OPEN, kernel)
+
+        # Threshold and find contours
+        ret, thresh = cv2.threshold(binary_image, 127, 255, 0)
+        binary_image, contours, hierarchy = cv2.findContours(thresh, 1, 2)
+
+        # Get bounding box from contours
         maxcnt = []
         for i in contours:
-            if(cv2.contourArea(i) > 1000):
+            if cv2.contourArea(i) > 1000:
                 maxcnt.append(i)
+
+        # Draw coutours on the image
         for i in maxcnt:
             rect = cv2.minAreaRect(i)
             box = cv2.boxPoints(rect)
             box = np.int0(box)
-            cv2.drawContours(self.cv_image,[box],0,(0,0,255),2)
-        if display:
-            cv2.imshow("HSV image",hsv_image)
-        #cv2.waitKey(0)
+            cv2.drawContours(image, [box], 0, (0, 0, 255), 2)
 
         print contours
 
         if display:
-            cv2.imshow("Binary Image BRG",binary_image3)
-            cv2.imshow("Binary Image HSV",binary_image4)
-            cv2.imshow("Binary Image With Morphology",binary_image6)
-            cv2.imshow("images",self.cv_image)
+            cv2.imshow("Original images", image)
+            cv2.imshow("HSV image", hsv_image)
+            cv2.imshow("Binary Image With Morphology", binary_image)
             cv2.waitKey(0)
-        return maxcnt, binary_image6
+        return maxcnt, binary_image
+
+    def get_green_bounding_boxes(self, hsv_image, rgb_image):
+        binary_image1 = cv2.inRange(hsv_image, np.array([40, 30, 30], dtype="uint8"),
+                                    np.array([100, 255, 255], dtype="uint8"))
+        return cv2.bitwise_or(binary_image1, binary_image1)
+
+    def get_red_bounding_boxes(self, hsv_image, rgb_image):
+        binary_image1 = cv2.inRange(hsv_image, np.array([0, 100, 0], dtype="uint8"),
+                                    np.array([10, 255, 255], dtype="uint8"))
+        binary_image2 = cv2.inRange(hsv_image, np.array([170, 100, 0], dtype="uint8"),
+                                    np.array([180, 255, 255], dtype="uint8"))
+        binary_image3 = cv2.inRange(rgb_image, np.array([80, 80, 250], dtype="uint8"),
+                                    np.array([220, 230, 255], dtype="uint8"))
+        binary_image4 = cv2.bitwise_or(binary_image1, binary_image2)
+        return cv2.bitwise_or(binary_image3, binary_image4)
+
+
 def set_red_lower_bound(self, val):
-        """ A callback function to handle the OpenCV slider to select the red lower bound """
-        self.red_lower_bound = val
+    """ A callback function to handle the OpenCV slider to select the red lower bound """
+    self.red_lower_bound = val
+
+
 def testImageFromFile(filename):
     tracker = ColorTracker()
     # TODO: Load image
-    tracker.find_bounding_boxes(filename)
+    tracker.find_bounding_boxes(filename, True)
     # TODO: Display result
+
 
 if __name__ == '__main__':
     colortracker = ColorTrackerROS()
