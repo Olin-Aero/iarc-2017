ef08ae517b8fbf2e9e1b98095c7ac23f407e81fc
nathanestill <nathan.estill@students.olin.edu>
2018-03-10 14:38:14 -0500

    adding centers of boxes

--- iarc_vision/scripts/colortracking.py
+++ iarc_vision/scripts/colortracking.py
@@ -13,21 +13,24 @@ Owner:
 
 import rospy
 import sys
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 import cv2
+from image_geometry import PinholeCameraModel
+from tf import TransformListener
 
 class ColorTrackerROS(object):
     def __init__(self):
         rospy.init_node('color_tracker')
-
         self.tracker = ColorTracker()
-
-
         self.cv_image = self.processed_image = None     # the latest image from the camera
+        self.boxes = []
         self.bridge = CvBridge() # used to convert ROS messages to OpenCV
+        self.cameraModel = PinholeCameraModel()
+        self.tf = TransformListener()
         rospy.Subscriber("/ardrone/bottom/image_raw", Image, self.process_image)
+        rospy.Subscriber("/ardrone/bottom/camera_info", CameraInfo, self.on_camera_info)
         print("Initializing Color Tracker")
         cv2.namedWindow('preview_window')
         cv2.namedWindow('binary')
@@ -42,7 +45,10 @@ class ColorTrackerROS(object):
             self.cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
             # do image processing here
             self.boxes, self.processed_image = self.tracker.find_bounding_boxes(self.cv_image, display=False)
-            
+            print(self.boxes)
+            for box in self.boxes:
+                center = np.mean(box,axis = 0)
+                print(center)
             # TODO: Do something with the boxes
             # TODO: make sure it doesn't get too far behind
 
@@ -52,13 +58,15 @@ class ColorTrackerROS(object):
             print "Error loading image"
             print(e)
 
+    def on_camera_info(self,msg):
+        self.cameraModel.fromCameraInfo(msg)
+
     def run(self):
         """ The main run loop, in this node it doesn't do anything """
         r = rospy.Rate(5)
         while not rospy.is_shutdown():
             # start out not issuing any motor commands
             if not self.cv_image is None:
-                print self.cv_image.shape
                 cv2.imshow('preview_window', self.cv_image)
                 cv2.imshow('binary', self.processed_image)
                 cv2.waitKey(5)
@@ -84,23 +92,25 @@ class ColorTracker(object):
         binary_image6 = cv2.morphologyEx(binary_image5, cv2.MORPH_OPEN, kernel)
         #cv2.imshow("images",binary_image)
         #cv2.waitKey(0)
-        ret,thresh = cv2.threshold(binary_image3,127,255,0)
+        ret,thresh = cv2.threshold(binary_image6,127,255,0)
         image,contours,hierarchy = cv2.findContours(thresh,1,2)
         #cnt = countours[0]
         maxcnt = []
         for i in contours:
-            if(cv2.contourArea(i) > 1000):
+            if(cv2.contourArea(i) > 10000):
                 maxcnt.append(i)
+        boxes = []
         for i in maxcnt:
             rect = cv2.minAreaRect(i)
             box = cv2.boxPoints(rect)
             box = np.int0(box)
+            boxes.append(box)
             cv2.drawContours(self.cv_image,[box],0,(0,0,255),2)
         if display:
             cv2.imshow("HSV image",hsv_image)
         #cv2.waitKey(0)
 
-        print contours
+        #print contours
 
         if display:
             cv2.imshow("Binary Image BRG",binary_image3)
@@ -108,7 +118,7 @@ class ColorTracker(object):
             cv2.imshow("Binary Image With Morphology",binary_image6)
             cv2.imshow("images",self.cv_image)
             cv2.waitKey(0)
-        return maxcnt, binary_image6
+        return boxes, binary_image6
 def set_red_lower_bound(self, val):
         """ A callback function to handle the OpenCV slider to select the red lower bound """
         self.red_lower_bound = val
