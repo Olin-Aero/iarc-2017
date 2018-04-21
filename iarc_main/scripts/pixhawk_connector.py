d2658df504583dd13b37b0016f72e77eb04d9eb3
Eric Miller <eric@legoaces.org>
2018-04-21 16:23:24 -0400

    Added untested code to compute and publish vehicle location

--- iarc_main/scripts/pixhawk_connector.py
+++ iarc_main/scripts/pixhawk_connector.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python2
 
 import rospy
-from geometry_msgs.msg import Twist, PoseStamped
+from geometry_msgs.msg import Twist, PoseStamped, Vector3Stamped
 from mavros_msgs.srv import SetMode, CommandTOL
+from nav_msgs.msg import Odometry
 from std_msgs.msg import Empty
+
+from numpy import sin, cos
+
+from tf import TransformBroadcaster
+import tf.transformations
 
 
 class PixhawkConnector(object):
@@ -18,12 +24,50 @@ class PixhawkConnector(object):
 
         rospy.loginfo('Services connected')
 
+        self.tfb = TransformBroadcaster()
+
         self.vel = Twist()
         self.vel_sub = rospy.Subscriber('/cmd_vel', Twist, self.on_vel)
 
         self.takeoff_sub = rospy.Subscriber('/takeoff', Empty, self.on_takeoff)
         self.land_sub = rospy.Subscriber('/land', Empty, self.on_land)
 
+        self.last_pos = Vector3Stamped()
+        self.position_sub = rospy.Subscriber('/mavros/global_position/local', Odometry, self.on_pose)
+
+    def on_pose(self, msg):
+        """
+        :param Odometry msg:
+        """
+        vel = msg.twist.twist.linear
+        alt = msg.pose.pose.position.z
+        orientation = msg.pose.pose.orientation
+        _, _, yaw = tf.transformations.euler_from_quaternion(orientation)
+
+        # TODO: check this math for trig errors
+        dt = (msg.header.stamp - self.last_pos.header.stamp).to_sec()
+
+        self.last_pos.vector.x += dt * (vel.x * cos(yaw) + vel.y * sin(yaw))
+        self.last_pos.vector.y += dt * (vel.y * cos(yaw) - vel.x * sin(yaw))
+        self.last_pos.vector.z = alt
+
+        self.last_pos.header.stamp = msg.header.stamp
+
+        pose = PoseStamped()
+
+        pose.header.stamp = msg.header.stamp
+        pose.header.frame_id = 'odom'
+
+        pose.pose.orientation = orientation
+        pose.pose.position.z = self.last_pos.vector
+
+        self.publish_pose(pose)
+
+    def publish_pose(self, pose):
+        print "Calculated pose: ", pose
+        self.tfb.sendTransform(pose.pose.position, pose.pose.orientation, pose.header.stamp, 'fcu',
+                               pose.header.frame_id)
+        # TODO: Publish Odometry message as well
 
     def setup_pixhawk(self):
         ok = self.set_mode(custom_mode='GUIDED')
